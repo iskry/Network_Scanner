758e2e3d1ebc0910f8bca4f4fdd8f3a8b2cfee62
iskry <refocuslucifer@protonmail.com>
2023-08-13 20:36:11 -0400

    expanded functionality to be more verbose for debugging

--- network_scanner.py
+++ network_scanner.py
@@ -4,8 +4,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    print(broadcast.summary())
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 scan("192.168.122.1/24")
 
