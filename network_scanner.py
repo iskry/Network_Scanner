a1949464d0ffa1071886116cb9b1f5e4b9dda489
iskry <refocuslucifer@protonmail.com>
2023-08-13 20:30:47 -0400

    used scapy library to construct an ethernet frame with the dest mac address set to broadcast to all devices

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,8 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    print(broadcast.summary())
 
 scan("192.168.122.1/24")
 
